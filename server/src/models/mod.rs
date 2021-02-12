4630c06f161cfd3ea5c3af4ab5a775235b595fc2
Ruman <rumanbsl@gmail.com>
2021-02-13 00:47:07 +0200

    pgAdmin integration

--- server/src/models/mod.rs
+++ server/src/models/mod.rs
@@ -1,17 +1,23 @@
-use serde::Serialize;
-use sqlx::{FromRow, Type};
+use sqlx::{types::chrono, FromRow, Type};
 
-#[derive(FromRow, Debug, Serialize)]
+#[derive(FromRow, Debug)]
 pub struct User {
   pub id: i64,
   pub name: String,
   pub active: Active,
+  pub created: chrono::DateTime<chrono::Utc>,
 }
-#[derive(Type, Debug, Serialize)]
+#[derive(Type, Debug)]
 #[sqlx(type_name = "active", rename_all = "SCREAMING_SNAKE_CASE")]
 pub enum Active {
   Unspecified,
   OffLine,
   Active,
   NotSeen,
+}
+
+impl std::default::Default for Active {
+  fn default() -> Self {
+    Active::Unspecified
+  }
 }
